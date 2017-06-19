66284e671a8b94fe4e897160e2f81f3a3c3b16a8
zcong1993 <1005997919@qq.com>
2017-06-19 22:06:32 +0800

    update build script and add update notify

--- version.go
+++ version.go
@@ -1,15 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	latest "github.com/tcnksm/go-latest"
+	"time"
+	"bytes"
+)
 
 // Name is cli name
 const Name = "rls"
 
 // Version is cli current version
-const Version string = "v0.1.0"
+const Version string = "v0.2.0"
 
 // GitCommit is cli current git commit hash
 var GitCommit string
+
+const defaultCheckTimeout = 2 * time.Second
 
 // ShowVersion is handler for version command
 func ShowVersion() {
@@ -18,4 +25,32 @@ func ShowVersion() {
 		version += fmt.Sprintf(" (%s)", GitCommit)
 	}
 	fmt.Println(version)
+	var buf bytes.Buffer
+	verCheckCh := make(chan *latest.CheckResponse)
+	go func() {
+		fixFunc := latest.DeleteFrontV()
+		githubTag := &latest.GithubTag{
+			Owner:             "zcong1993",
+			Repository:        "git-release",
+			FixVersionStrFunc: fixFunc,
+		}
+
+		res, err := latest.Check(githubTag, fixFunc(Version))
+		if err != nil {
+			// Don't return error
+			return
+		}
+		verCheckCh <- res
+	}()
+
+	select {
+	case <-time.After(defaultCheckTimeout):
+	case res := <-verCheckCh:
+		if res.Outdated {
+			fmt.Fprintf(&buf,
+				"Latest version of rls is v%s, please upgrade!\n",
+				res.Current)
+		}
+	}
+	fmt.Print(buf.String())
 }
